0c794acd6e48662e3f2bf77038458406f9a9d95c
Safonov85 <safonov85@yahoo.com>
2017-05-15 21:38:41 +0200

    First commit

---
+++ ShortestPath/Main.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ShortestPath
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(12, 12);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(129, 68);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(530, 430);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 510);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "Main";
+            this.Text = "ShortestPath";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ ShortestPath/Main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortestPath
+{
+    public partial class Main : Form
+    {
+        Bitmap bitmap;
+        Graphics graphics;
+        Graphics graphicsOverride;
+        Color backgroundColor = Color.FromArgb(250, 250, 250);
+        Pen pen = new Pen(Color.Red, 3.0F);
+        Random ran = new Random();
+        int[] placesPositionX;
+        int[] placesPositionY;
+
+
+        public Main()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Instance();
+            DrawNewFrame();
+        }
+
+        void DrawNewFrame()
+        {
+            graphics.Clear(backgroundColor);
+
+            int amount = 0;
+            while(amount < placesPositionX.Length - 1)
+            {
+                graphics.DrawEllipse(pen, placesPositionX[amount], placesPositionY[amount], 20, 20);
+                amount++;
+            }
+
+            graphicsOverride.DrawImage(bitmap, 0, 0, pictureBox.Width, pictureBox.Height);
+        }
+
+        void Instance()
+        {
+            bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(bitmap);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphicsOverride = pictureBox.CreateGraphics();
+            placesPositionX = new int[10];
+            placesPositionY = new int[10];
+
+            int amount = 0;
+            foreach(int num in placesPositionX)
+            {
+                placesPositionX[amount] = RandomNumber();
+                amount++;
+            }
+
+            amount = 0;
+            foreach (int num in placesPositionY)
+            {
+                placesPositionY[amount] = RandomNumber();
+                amount++;
+            }
+
+
+        }
+
+        int RandomNumber()
+        {
+            int ranNum = ran.Next(10, 400);
+            return ranNum;
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            Instance();
+            DrawNewFrame();
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
